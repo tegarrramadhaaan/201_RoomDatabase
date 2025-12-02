6ff972d7f66ae3ce16708c33b3f0ab08e5decac6
tegarrramadhaaan <anakrama2000@gmail.com>
2025-12-02 14:33:10 +0700

    menambahkan code SiswaDao.kt

--- app/src/main/java/com/example/myroomsatu/room/SiswaDao.kt
+++ app/src/main/java/com/example/myroomsatu/room/SiswaDao.kt
@@ -6,11 +6,3 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
-@Dao
-interface SiswaDao {
-    @Query("SELECT * from tblsiswa ORDER BY nama ASC")
-    fun getAllSiswa(): Flow<List<Siswa>>
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(siswa: Siswa)
-}
