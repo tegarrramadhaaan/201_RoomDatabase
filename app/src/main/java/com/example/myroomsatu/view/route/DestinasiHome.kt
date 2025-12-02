ffe6ff228de2305a78d82e5fe71880915cf523ed
tegarrramadhaaan <anakrama2000@gmail.com>
2025-12-02 14:31:04 +0700

    menambahkan code destinasi entry

--- app/src/main/java/com/example/myroomsatu/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiHome.kt
@@ -2,7 +2,3 @@ package com.example.myroomsatu.view.route
 
 import com.example.myroomsatu.R
 
-object DestinasiHome : DestinasiNavigasi {
-    override val route = "home"
-    override val titleRes = "Data Siswa" // This should typically be R.string.data_siswa
-}
