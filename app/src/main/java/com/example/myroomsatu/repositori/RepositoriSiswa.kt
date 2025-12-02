22362b00a2dedb4f2f6faa4a1e44943b5c5c6e1a
tegarrramadhaaan <anakrama2000@gmail.com>
2025-12-02 14:29:35 +0700

    menambahkan code

--- app/src/main/java/com/example/myroomsatu/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/repositori/RepositoriSiswa.kt
@@ -10,13 +10,4 @@ interface RepositoriSiswa {
     suspend fun insertSiswa(siswa: Siswa): Any
 }
 
-class OfflineRepositoriSiswa(
-    private val siswaDao: SiswaDao
-) : RepositoriSiswa {
 
-    override fun getAllSiswaStream(): Flow<List<Siswa>> =
-        siswaDao.getAllSiswa()
-
-    override suspend fun insertSiswa(siswa: Siswa) =
-        siswaDao.insert(siswa)
-}
